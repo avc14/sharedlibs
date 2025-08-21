fcf2f0e84de9e8ec43c67fc8f7997f25fbc85ce8
amol chavan <87474532+avc14@users.noreply.github.com>
2025-08-21 08:38:44 +0530

    Update createTags.groovy

--- vars/createTags.groovy
+++ vars/createTags.groovy
@@ -1,6 +1,6 @@
 def call(Map args){
   def version = createTag(args.base ?: "app")
-  bat "echo %version%"
+  bat "echo ${version}"
 }
 private def createTag(String base) {
   def ts = new Date().format("yyyyMMddHHmmss")
