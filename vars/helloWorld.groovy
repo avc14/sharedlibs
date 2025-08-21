b8cebc44cd8f527addcd36a6d291d05faa6a7bf3
amol chavan <87474532+avc14@users.noreply.github.com>
2025-08-21 08:23:44 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,14 @@
-def call() {
+def call(String name, String env) {
+    check_args($name,$env)
     bat "echo my name is number"
+}
+
+private def check_args(String arg1, String arg2) {
+    if (arg1 == 'AMOL') {
+        if (arg2 == 'DEV'){
+            bat 'echo correct runner'
+        }
+    } else {
+        error('incorrect runner or env')
+    }
 }
