fdb4089c9d1ead0e4148c86c0b70a232e52fea96
amol chavan <87474532+avc14@users.noreply.github.com>
2025-08-22 09:23:06 +0530

    Update installDep.groovy

--- vars/installDep.groovy
+++ vars/installDep.groovy
@@ -1,11 +1,18 @@
-def call(){
-  try {
-    bat """
-    python -m venv venv
-    call venv/Scripts/activate
-    pip install -r requirements.txt
-    """
-  } catch ( Exception e ) {
-    error("Dependency installation failed with error : ${e.message}")
+def call(Map args = [:]) {
+  def python = args.get("python","python")
+  def requirements = args.get("requirements","requirements.txt")
+
+  if (!fileExists(requirements)) {
+    error "Requirements file is not present"
   }
+
+  if (!fileExists("venv/Scripts/activate.bat") {
+    bat "${python} -m venv venv"
+  }
+      
+  bat """
+  ${python} -m venv venv
+  call venv/Scripts/activate
+  pip install -r ${requirements}
+  """
 }
